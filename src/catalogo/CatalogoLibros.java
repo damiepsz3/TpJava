b0da3b22b5b339282e594058cb18491caee066c9
Damian Epsztein <damianepsztein@Mac-mini-de-Damian.local>
2016-12-06 12:10:48 -0300

    agregado de funcion alta libro

--- src/catalogo/CatalogoLibros.java
+++ src/catalogo/CatalogoLibros.java
@@ -18,11 +18,11 @@ public class CatalogoLibros {
 		ResultSet rs = null;
 		ArrayList<Libro> lista = new ArrayList<Libro>();
 		try {
-			pst = con.prepareStatement("SELECT * FROM libros ORDER BY nombre");
+			pst = con.prepareStatement("SELECT * FROM libros ORDER BY titulo");
 			rs = pst.executeQuery();
 
 			while (rs.next()) {
-				Libro lib = new Libro(rs.getInt("idlibro"), rs.getString("nombre"), rs.getString("autor"));
+				Libro lib = new Libro(rs.getInt("idlibro"), rs.getString("titulo"), rs.getString("autor"));
 				lista.add(lib);
 			}
 
@@ -53,7 +53,44 @@ public class CatalogoLibros {
 
 	}
 
-	public void agregarLibro(Libro nuevoLibro) {
+	public void agregarLibro(Libro nuevoLibro) throws Exception {
+		Connection con = Conexion.getConexion();
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		con.setAutoCommit(false);
 
+		try {
+			pst = con.prepareStatement("INSERT INTO libros (titulo, autor)" + "VALUES (?,?)");
+			pst.setString(1, nuevoLibro.getTitulo());
+			pst.setString(2, nuevoLibro.getAutor());
+			pst.executeUpdate();
+		} catch (Exception e) {
+			con.rollback();
+			throw e;
+		} finally {
+			if (con != null) {
+				try {
+					con.commit();
+					con.setAutoCommit(true);
+					Conexion.cerrarConexion();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+			if (pst != null) {
+				try {
+					pst.close();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		}
 	}
 }
